b29547ed6d01fc9ed0efa2cdd96b1db1a062102b
jmzobitz <zobitz@augsburg.edu>
2018-12-25 21:00:47 -0600

    Reshuffle of some programs in the data processing.

--- data-raw/process-MCD43A3.R
+++ data-raw/process-MCD43A3.R
@@ -9,90 +9,76 @@ library(tidyverse)
 library(lubridate)
 library(devtools)
 
+
+# Pull in the black sky albedo and filter
 modisAlbedoData_BSA <- read_csv('data-raw/fluxnet-combined-albedo-black-sky/Fluxnet-Combined-Albedo-Black-Sky-MCD43A3-006-results.csv') %>%
   select(ID,Date,
-         MCD43A3_006_Albedo_BSA_Band1,
-         MCD43A3_006_Albedo_BSA_Band2,
-         MCD43A3_006_Albedo_BSA_Band3,
-         MCD43A3_006_Albedo_BSA_Band4,
-         MCD43A3_006_Albedo_BSA_Band5,
-         MCD43A3_006_Albedo_BSA_Band6,
-         MCD43A3_006_Albedo_BSA_Band7,
-         MCD43A3_006_BRDF_Albedo_Band_Mandatory_Quality_Band1,
-         MCD43A3_006_BRDF_Albedo_Band_Mandatory_Quality_Band2,
-         MCD43A3_006_BRDF_Albedo_Band_Mandatory_Quality_Band3,
-         MCD43A3_006_BRDF_Albedo_Band_Mandatory_Quality_Band4,
-         MCD43A3_006_BRDF_Albedo_Band_Mandatory_Quality_Band5,
-         MCD43A3_006_BRDF_Albedo_Band_Mandatory_Quality_Band6,
-         MCD43A3_006_BRDF_Albedo_Band_Mandatory_Quality_Band7)
+         matches("_Albedo_BSA_Band")) %>%
+  rename(time=Date,site=ID) %>%   # Rename the columns
+  mutate(time = yday(time)) %>%
+  gather(key=band_str,value=value,-time,-site) %>%  # Gather by site and time
+  mutate(kernel=str_extract(band_str,".$"),band=str_extract(band_str, "Band.")) %>% # Add columns for the kernel and reflectance band
+  mutate(band=str_to_lower(band)) %>%  # Convert to lower case
+  select(-band_str) %>%
+  arrange(site,time,kernel)
 
+# Pull in the white sky albedo and filter
+modisAlbedoData_WSA <- read_csv('data-raw/fluxnet-combined-albedo-black-sky/Fluxnet-Combined-Albedo-Black-Sky-MCD43A3-006-results.csv') %>%
+  select(ID,Date,
+         matches("_Albedo_WSA_Band")) %>%
+  rename(time=Date,site=ID) %>%   # Rename the columns
+  mutate(time = yday(time)) %>%
+  gather(key=band_str,value=value,-time,-site) %>%  # Gather by site and time
+  mutate(kernel=str_extract(band_str,".$"),band=str_extract(band_str, "Band.")) %>% # Add columns for the kernel and reflectance band
+  mutate(band=str_to_lower(band)) %>%  # Convert to lower case
+  select(-band_str) %>%
+  arrange(site,time,kernel)
+
+
+modisAlbedo_flag <- read_csv('data-raw/fluxnet-combined-albedo-black-sky/Fluxnet-Combined-Albedo-Black-Sky-MCD43A3-006-results.csv') %>%
+  select(ID,Date,
+         matches("_Mandatory_Quality_Band")) %>%
+  select(-matches("bitmask|MODLAND|Fill")) %>% # Remove the extra pieces
+  rename(time=Date,site=ID) %>%   # Rename the columns
+  mutate(time = yday(time)) %>%
+  gather(key=band_str,value=flag,-time,-site) %>%
+  mutate(band=str_extract(band_str, "Band.$")) %>%  # Search for the end of string
+  mutate(band=str_to_lower(band)) %>%
+  select(-band_str)
+
+
+# From the APPEARS webpage:
+# *Mandatory QA bit index:
+#   0 = processed, good quality (full BRDF inversions)
+# 1 = processed, see other QA (magnitude BRDF inversions)
+# 255 = Fill Value
 
 
 # here if QA = 0, we have good quality.
 # only take if we have QA = 0 across all bands
 
-flagValues = modisAlbedoData_BSA %>% select(10,16) %>% rowSums()
+# Now join the two datasets together, filter out good flags
+BSA_filtered <- modisAlbedoData_BSA %>%
+  left_join(modisAlbedo_flag,by=c("site","time","band")) %>%
+  filter(flag==0) %>%
+  select(-flag) %>%
+  mutate(type='black-sky')
 
-mcd43A3_BlackSkyAlbedo <- modisAlbedoData_BSA %>% mutate(flagValues) %>%
-  filter(flagValues==0) %>%
-  select(1:9) %>%
-  rename("1"=MCD43A3_006_Albedo_BSA_Band1,
-         "2"=MCD43A3_006_Albedo_BSA_Band2,
-         "3"=MCD43A3_006_Albedo_BSA_Band3,
-         "4"=MCD43A3_006_Albedo_BSA_Band4,
-         "5"=MCD43A3_006_Albedo_BSA_Band5,
-         "6"=MCD43A3_006_Albedo_BSA_Band6,
-         "7"=MCD43A3_006_Albedo_BSA_Band7,
-         site=ID) %>%
-  mutate(time=yday(Date),method="mcd43A3",type="black-sky") %>%
-  select(-Date) %>%
-  gather(key=band,value=bs,"1","2","3","4","5","6","7")
+# Now join the two datasets together, filter out good flags
+WSA_filtered <- modisAlbedoData_WSA %>%
+  left_join(modisAlbedo_flag,by=c("site","time","band")) %>%
+  filter(flag==0) %>%
+  select(-flag) %>%
+  mutate(type='white-sky')
 
 
 ### Now do white sky albedo
 # Read in APPEARS albedo data for comparison
 
 
-modisAlbedoData_WSA <- read_csv('data-raw/fluxnet-combined-albedo-black-sky/Fluxnet-Combined-Albedo-Black-Sky-MCD43A3-006-results.csv') %>%
-  select(ID,Date,
-         MCD43A3_006_Albedo_WSA_Band1,
-         MCD43A3_006_Albedo_WSA_Band2,
-         MCD43A3_006_Albedo_WSA_Band3,
-         MCD43A3_006_Albedo_WSA_Band4,
-         MCD43A3_006_Albedo_WSA_Band5,
-         MCD43A3_006_Albedo_WSA_Band6,
-         MCD43A3_006_Albedo_WSA_Band7,
-         MCD43A3_006_BRDF_Albedo_Band_Mandatory_Quality_Band1,
-         MCD43A3_006_BRDF_Albedo_Band_Mandatory_Quality_Band2,
-         MCD43A3_006_BRDF_Albedo_Band_Mandatory_Quality_Band3,
-         MCD43A3_006_BRDF_Albedo_Band_Mandatory_Quality_Band4,
-         MCD43A3_006_BRDF_Albedo_Band_Mandatory_Quality_Band5,
-         MCD43A3_006_BRDF_Albedo_Band_Mandatory_Quality_Band6,
-         MCD43A3_006_BRDF_Albedo_Band_Mandatory_Quality_Band7)
 
-
-
-# here if QA = 0, we have good quality.
-# only take if we have QA = 0 across all bands
-
-flagValues = modisAlbedoData_WSA %>% select(10,16) %>% rowSums()
-
-mcd43A3_WhiteSkyAlbedo <- modisAlbedoData_WSA %>% mutate(flagValues) %>%
-  filter(flagValues==0) %>%
-  select(1:9) %>%
-  rename("1"=MCD43A3_006_Albedo_WSA_Band1,
-         "2"=MCD43A3_006_Albedo_WSA_Band2,
-         "3"=MCD43A3_006_Albedo_WSA_Band3,
-         "4"=MCD43A3_006_Albedo_WSA_Band4,
-         "5"=MCD43A3_006_Albedo_WSA_Band5,
-         "6"=MCD43A3_006_Albedo_WSA_Band6,
-         "7"=MCD43A3_006_Albedo_WSA_Band7,
-         site=ID) %>%
-  mutate(time=yday(Date),method="mcd43A3",type="white-sky") %>%
-  select(-Date) %>%
-  gather(key=band,value=bs,"1","2","3","4","5","6","7")
-
-mcd43A3 <- rbind(mcd43A3_BlackSkyAlbedo,mcd43A3_WhiteSkyAlbedo)
+mcd43A3 <- rbind(BSA_filtered,WSA_filtered)
 
 use_data(mcd43A3,overwrite = TRUE)
+
 
